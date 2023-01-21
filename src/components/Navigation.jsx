4fc12ef8e52780b713253178761ddbf8432e3bee
AhnafAhamed <ahnafahamed4@gmail.com>
2023-01-21 09:49:00 +0530

    feat: add active styles to navigation

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,20 +1,26 @@
 import styles from "@/styles/components/Navigation.module.css";
 import Link from "next/link";
+import { useRouter } from "next/router";
 
-const Navigation = () => (
-  <nav className={styles.navigationBar}>
+const Navigation = () => {
+  const router = useRouter();
+  const currentRoute = router.pathname;
+
+  return (
+    <nav className={styles.navigationBar}>
     <ul className={styles.navigationList}>
       <li className={styles.navigationListItem}>
-        <Link href="/">Home</Link>
+        <Link href="/" className={currentRoute === '/' ? styles.active : ''}>Home</Link>
       </li>
       <li className={styles.navigationListItem}>
-        <Link href="/about">About</Link>
+        <Link href="/about">Projects</Link>
       </li>
       <li className={styles.navigationListItem}>
-        <Link href="/contact">Contact</Link>
+        <Link href="/contact">Blogs</Link>
       </li>
     </ul>
   </nav>
-);
+  )
+};
 
 export default Navigation;
